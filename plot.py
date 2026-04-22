5f8cc2862638fa845b0f40fa5f8ab9485ba3b1ec
lyy1119 <me@lycarus.cn>
2026-04-22 09:53:56 +0800

    add feature duncan-chang model fit

--- plot.py
+++ plot.py
@@ -3,6 +3,7 @@ from collections.abc import Iterable
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from scipy.stats import linregress
 
 NumericData = int | float | Iterable[int | float]
 
@@ -14,17 +15,25 @@ def plot(f, xMin, xMax, number):
     plt.plot(xList, yList, '-', color='black', label='z stress')
 
 if __name__ == "__main__":
-    data = pd.read_csv('s.csv')
-    x = [float(i) for i in data['Step'].values]
-    y = [float(i) for i in data['zStress'].values]
-    with plot_style('my plot', 'step', 'stress'):
-        plt.scatter(x, y, color='black', label='origin')
+    # confinePressure = 800000.0
+    confinePressure = float(input('input confining pressue (Pa):'))
+    csvfile = input('input csv file:').strip()
+    csvfile = csvfile if csvfile.endswith('.csv') else csvfile+'.csv'
+    data = pd.read_csv('800000.0.csv')
+    deltaStress = [float(i)-confinePressure for i in data['stress'].values]
+    strain = [float(i) for i in data['strain'].values]
+    y = []
+    for i, j in zip(deltaStress, strain):
+        y.append(j/i)
+    x = strain
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
 
-    # def f(x: NumericData):
-    #     y = lambda x: x / (-4498.78659 + 0.06865 * x)
-
-    #     return y(x)
-
-    # with plot_style('my plot', 'step', 'stress'):
-    #     print(1)
-    #     plot(f, 65540, 865540, 800)
+    with plot_style('my plot', r'$\epsilon$', r'$\frac{\epsilon}{\Delta \sigma}$'):
+        plt.scatter(x, y, color='black', label='origin', s=3)
+        yLinear = [slope*i + intercept for i in x] # type: ignore
+        plt.plot(x, yLinear, '-', color='red')
+    print(f'sigma_1 - sigma_3 = {1e100/(intercept+slope*1e100)}') # type: ignore
+    with plot_style('origin', r'$\epsilon$', r'$\Delta \sigma$'):
+        plt.scatter(strain, deltaStress, color='black', label='origin', s=3)
+        deltaStressFit = [i/(intercept + slope*i) for i in strain] # type: ignore
+        plt.plot(strain, deltaStressFit, '-', color='red')
